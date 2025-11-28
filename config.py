61b43aab86c162a946b31dbd4d410e7a94df1b24
jumanazarbot-tech <jumanazarbot@gmail.com>
2025-11-28 11:03:24 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+TOKEN = "SIZNING_BOT_TOKEN_HERE"
+ADMIN_IDS = {123456789}  # o'zing admin ID lar
+PAYMENTS_CHANNEL = "@your_payments_channel"   # to'lovlar kanali (virtual)
+FORCE_SUB_CHANNELS = ["@your_required_channel"]  # majburiy obuna kanallari
+
+# ekonomika
+REF_BONUS = 0.00125
+MIN_WITHDRAW = 0.005
+DAILY_BONUS = 0.0005
+
+DB_PATH = "bot.db"
